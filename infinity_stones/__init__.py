6fc5fbf54685d74910638b65dcaf81722361feb8
casey <GLACIER.EQUILIBRIUM@GMAIL.COM>
2026-08-06 23:56:16 -1000

    synth(stones): export projection API

--- infinity_stones/__init__.py
+++ infinity_stones/__init__.py
@@ -8,6 +8,13 @@ from .models import (
     StoneManifest,
     UpgradeManifest,
 )
+from .projections import (
+    ProjectionLayer,
+    StoneProjection,
+    build_projection,
+    build_projection_bundle,
+    canonical_stone_payload,
+)
 from .psysoc_x import calibrate
 from .registry import StoneRegistry
 
@@ -15,9 +22,14 @@ __all__ = [
     "AudienceContext",
     "CalibrationProfile",
     "CompositionPlan",
+    "ProjectionLayer",
     "StoneManifest",
+    "StoneProjection",
     "StoneRegistry",
     "UpgradeManifest",
+    "build_projection",
+    "build_projection_bundle",
     "calibrate",
+    "canonical_stone_payload",
     "compose_loadout",
 ]
